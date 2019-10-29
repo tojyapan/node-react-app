dcb32d5e66464324dfdc91c0590011457c929456
Toya Kawakami <tojyapan925@gmail.com>
2019-10-29 12:22:23 +0900

    Apply Redux and reducers auth

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>
+
+const App = () => {
+  return (
+    <div>
+      Hi There!
+    </div>
+  );
+};
+
+export default App;
