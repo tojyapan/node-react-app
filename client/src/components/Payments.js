e17d4f90dcdac422e8995792228d8be022b51c3a
Toya Kawakami <tojyapan925@gmail.com>
2019-10-30 16:45:09 +0900

    Input Credit info form in header

---
+++ client/src/components/Payments.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Emaily"
+        description="¥500 for 5email credits"
+        amount={500}
+        currency="JPY"
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">
+          Add Credits
+        </button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default Payments;
