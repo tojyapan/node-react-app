af1dd81b05dba8f25f354508a1275b8450fc47eb
Toya Kawakami <tojyapan925@gmail.com>
2019-10-31 23:16:29 +0900

    Create input form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,11 +1,29 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' }
+];
 
 class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, field => {
+      return <Field key={field.name} component={SurveyField} type="text" label={field.label} name={field.name} />
+    });
+  }
+
   render() {
     return (
       <div>
-        SurveyForm
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
