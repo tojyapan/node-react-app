045b354aa0f2d22a2c86cf367582eda6d76d6570
Toya Kawakami <tojyapan925@gmail.com>
2019-11-01 17:36:07 +0900

    Send a survey for recipients

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,9 +2,11 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../actions';
 
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = _.map(formFields, field => {
     return (
       <div key={field.name}>
@@ -24,7 +26,7 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
       <button className="yellow white-text darken-3 btn-flat" onClick={onCancel} >
         Back
       </button>
-      <button className="green white-text btn-flat right">
+      <button onClick={() => submitSurvey(formValues, history)} className="green white-text btn-flat right">
         Send Survey
         <i className="material-icons right">email</i>
       </button>
@@ -36,4 +38,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
